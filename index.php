d602a57939fd28b1cf87923597bf0d05d3367a12
Gérald Leban <gerald.leban0@gmail.com>
2024-12-08 18:31:41 +0100

    feat: add accueil page with stats and styles

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+session_status();
+
+define("ROOT_DIR", "");
+require_once ROOT_DIR . 'includes/database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Accueil</title>
+
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/franken-ui@1.1.0/dist/css/core.min.css" />
+
+  <link
+    rel="stylesheet"
+    href="<?php echo ROOT_DIR . 'assets/css/globals.css' ?>" />
+
+  <link
+    rel="stylesheet"
+    href="<?php echo ROOT_DIR . 'assets/css/dashboard.styles.css' ?>" />
+
+  <link
+    rel="stylesheet"
+    href="<?php echo ROOT_DIR . 'assets/css/header.css' ?>" />
+</head>
+
+<body>
+  <?php include ROOT_DIR . 'includes/partials/header.php'; ?>
+
+  <main>
+    <div class="headings container">
+      <h1>Accueil</h1>
+      <p>
+        Bienvenue sur votre tableau de bord. Vous pouvez consulter les statistiques
+        de vos joueurs, les matches joués, les matches gagnés et les matches perdus.
+      </p>
+    </div>
+
+    <div class="stats container">
+      <div class="stat">
+        <div>
+          <p class="stat-title text-sm">Joueurs inscrits</p>
+        </div>
+        <div>
+          <p class="stat-value text-2xl">45.2k</p>
+          <p class="stat-description">+20.1% depuis le mois dernier</p>
+        </div>
+      </div>
+
+      <div class="stat">
+        <div>
+          <p class="stat-title text-sm">Matches joués</p>
+        </div>
+        <div>
+          <p class="stat-value text-2xl">45.2k</p>
+          <p class="stat-description">+20.1% depuis le mois dernier</p>
+        </div>
+      </div>
+
+      <div class="stat">
+        <div>
+          <p class="stat-title text-sm">Matches gagnés</p>
+        </div>
+        <div>
+          <p class="stat-value text-2xl">45.2k</p>
+          <p class="stat-description">+20.1% depuis le mois dernier</p>
+        </div>
+      </div>
+
+      <div class="stat">
+        <div>
+          <p class="stat-title text-sm">Matches perdus</p>
+        </div>
+        <div>
+          <p class="stat-value text-2xl">45.2k</p>
+          <p class="stat-description">+20.1% depuis le mois dernier</p>
+        </div>
+      </div>
+    </div>
+
+  </main>
+</body>
+
+</html>
