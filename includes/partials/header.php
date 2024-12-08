d8d196bcc0d9e0ef35a96dd837940dc74b41eaea
Gérald Leban <gerald.leban0@gmail.com>
2024-12-08 18:31:20 +0100

    feat: add header partial with navigation and styles

---
+++ includes/partials/header.php
@@ -0,0 +1,18 @@
+<header>
+  <div class="container">
+    <nav>
+      <a href="accueil.php" class="uk-button uk-button-ghost">
+        Accueil
+      </a>
+      <a href="joueurs.php" class="uk-button uk-button-ghost">
+        Joueurs
+      </a>
+      <a href="equipes.php" class="uk-button uk-button-ghost">
+        Équipes
+      </a>
+      <a href="matches.php" class="uk-button uk-button-ghost">
+        Matchs
+      </a>
+    </nav>
+  </div>
+</header>
