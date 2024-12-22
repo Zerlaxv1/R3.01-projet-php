be637b870c65142f52900c937a6a4097b13aa74d
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2024-12-22 17:43:21 +0100

    feat: refactor de l'en-tête

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -1,22 +1,42 @@
 <header>
-  <div class="container uk-flex uk-flex-between">
-    <nav>
-      <a href="/" class="uk-button uk-button-ghost">
-        Accueil
-      </a>
-      <a href="/joueurs" class="uk-button uk-button-ghost">
-        Joueurs
-      </a>
-      <a href="/equipes" class="uk-button uk-button-ghost">
-        Équipes
-      </a>
-      <a href="/matches" class="uk-button uk-button-ghost">
-        Matchs
-      </a>
-    </nav>
-    <div>
-      <a href="/loggout.php" class="uk-button uk-button-danger">
-        Déconnexion
-      </a>
-  </div>
+  <nav class="uk-navbar-container">
+    <div class="uk-container">
+      <div class="uk-navbar">
+        <div class="uk-navbar-left">
+          <ul class="uk-navbar-nav">
+            <?php
+            $pages = [
+              'Accueil' => '/',
+              'Joueurs' => '/joueurs/',
+              'Équipes' => '/equipes/',
+              'Matchs' => '/matches/'
+            ];
+
+            $currentPath = $_SERVER['REQUEST_URI'];
+
+            foreach ($pages as $page => $link) {
+              $link = str_replace('index.php', '', $link);
+              $currentPath = str_replace('index.php', '', $currentPath);
+
+              if ($link === $currentPath) {
+                echo "<li class=\"uk-active\"><a href=\"$link\">$page</a></li>";
+              } else {
+                echo "<li><a href=\"$link\">$page</a></li>";
+              }
+            }
+            ?>
+          </ul>
+        </div>
+
+        <div class="uk-navbar-right">
+          <ul class="uk-navbar-nav">
+            <li>
+              <a href="/loggout.php">
+                Déconnexion
+              </a>
+            </li>
+        </div>
+      </div>
+    </div>
+  </nav>
 </header>
